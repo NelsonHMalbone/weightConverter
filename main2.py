fdd0964e56cdd932cc026fc5a159049a60f9eecf
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 15:30:13 -0400

    8/20/2024 added the inches and cm conversions

--- main2.py
+++ main2.py
@@ -1,7 +1,15 @@
 # Conversion for inches to cm
 # cm to inches
 
-measure = input("Enter a measurement: ")
+measure = float(input("Enter a measurement: "))
 # unit of measurement
-uom = input("Convert Measurement (cm or in)")
+uom = input("Convert Measurement (cm or in): ")
 
+if uom == "cm":
+    measure = round(measure * 2.54, 3)
+    unit = 'centimeters'
+    print(f"measurement is {measure} {unit}")
+elif uom == "in":
+    measure = round(measure / 2.54, 3)
+    unit = 'inches'
+    print(f"measurement is {measure} {unit}")
